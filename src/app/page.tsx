be29f4ae0ad4cb245b7cde27a924f23cec9554a7
Dexter <coder0xh@gmail.com>
2024-11-27 11:44:47 +0800

    update bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Hero from '@/components/Hero'
 import About from '@/components/About'
-import Skills from '@/components/Skills'
 import Projects from '@/components/Projects'
 import Contact from '@/components/Contact'
 import Navbar from '@/components/Navbar'
@@ -11,7 +10,6 @@ export default function Home() {
       <Navbar />
       <Hero />
       <About />
-      <Skills />
       <Projects />
       <Contact />
     </main>
